ab4fd00f83bf37d7a3349f237fbaa6ddbecf235d
ganghuigeon <ganghuigeon@naver.com>
2024-02-08 17:09:44 +0900

    if, else

--- src/ch03/sec05/InfinityAndNanExample.java
+++ src/ch03/sec05/InfinityAndNanExample.java
@@ -4,7 +4,7 @@ public class InfinityAndNanExample {
     public static void main(String[] args) {
         int x = 5;
         double y = 0.0;
-        double z = x / y;
+        double z = x % y;
 
         System.out.println("z: " + z);
         
